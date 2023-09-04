dde11ae11cf2d7596508e62e016d1023e12334be
FelipeBertol <135773873+FelipeBertol@users.noreply.github.com>
2023-09-04 20:54:13 -0300

    Add files via upload

--- js/PieceClasses.js
+++ js/PieceClasses.js
@@ -1,18 +1,22 @@
-
-
 class Piece extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y, key, allowedMoves) {
+  constructor(scene, x, y, key, allowedMoves, allowedAttacks, player, life) {
     super(scene, x, y, key);
     this.allowedMoves = allowedMoves;
+    this.allowedAttacks = allowedAttacks;
+    this.player = player;
+    this.life = life;
   }
 }
 
-let direction = 0;
-let possibleAttacks = 0;
+let allowedMoves = 0;
+let allowedAttacks = 0;
+
+let matrizM = 0;
+let matrizA = 0;
 
 export class Crown extends Piece {
-  constructor(scene, x, y) {
-    direction = [
+  constructor(scene, x, y, turn) {
+    matrizM = [
       { row: 1, col: 0 }, 
       { row: 0, col: 1 },
       { row: 1, col: 1 },
@@ -21,75 +25,95 @@ export class Crown extends Piece {
       { row: -1, col: -1 },
       { row: -1, col: 0 },
       { row: 0, col: -1 }
-    ]
-    possibleAttacks = direction;
-    super(scene, x, y, 'crown', direction, possibleAttacks);
+    ];
+    matrizA = [
+      { rowA: 1, colA: 0 }, 
+      { rowA: 0, colA: 1 },
+      { rowA: 1, colA: 1 },
+      { rowA: 1, colA: -1 },
+      { rowA: -1, colA: 1 },
+      { rowA: -1, colA: -1 },
+      { rowA: -1, colA: 0 },
+      { rowA: 0, colA: -1 }
+    ];
+    if (turn == 0) {
+      allowedMoves = matrizM;
+      allowedAttacks = matrizA;
+    } else if (turn == 1) {
+      allowedMoves = matrizM;
+      allowedAttacks = matrizA;
+    }
 
+    
+    super(scene, x, y, 'crown', allowedMoves, allowedAttacks);
     scene.add.existing(this);
+    this.life = 1;
   }
 }
 
 export class Lance extends Piece {
   constructor(scene, x, y, turn) {
     if (turn == 0) {
-      direction = [
+      allowedMoves = [
         { row: 1, col: 0 }, 
         { row: 2, col: 0 }, 
         { row: 0, col: -1 },
         { row: 0, col: 1 }
       ]
-      possibleAttacks = [
-        {row: 1, col: 0},
-        {row: 2, col: 0}
+      allowedAttacks = [
+        { rowA: 1, colA: 0},
+        { rowA: 2, colA: 0}
       ]
 
     } else if (turn == 1) {
-      direction = [
+      allowedMoves = [
         { row: -1, col: 0 }, 
         { row: -2, col: 0 },
         { row: 0, col: -1 },
         { row: 0, col: 1 }
       ]
-      possibleAttacks = [
-        {row: -1, col: 0},
-        {row: -2, col: 0}
+      allowedAttacks = [
+        { rowA: -1, colA: 0},
+        { rowA: -2, colA: 0}
       ]
     }
-    super(scene, x, y, 'lance', direction, possibleAttacks);
+    super(scene, x, y, 'lance', allowedMoves, allowedAttacks);
     scene.add.existing(this);
+    this.life = 1;
   }
 }
 
 export class Sword extends Piece {
   constructor(scene, x, y, turn) {
     if (turn == 0) {
-      direction =[
+      allowedMoves =[
         { row: 1, col: 0 }, 
         { row: 0, col: -1 },
         { row: 0, col: 1 },
       ]
-      possibleAttacks = [
-        {row: 1, col: 0}
+      allowedAttacks = [
+        { rowA: 1, colA: 0 }
       ]
-
+      turn = 0
     } else if (turn == 1) {
-      direction =[
+      allowedMoves =[
         { row: -1, col: 0 }, 
         { row: 0, col: -1 },
         { row: 0, col: 1 },
       ]
-      possibleAttacks = [
-        {row: -1, col: 0}
+      allowedAttacks = [
+        { rowA: -1, colA: 0 }
       ]
     }
-    super(scene, x, y, 'sword', direction, possibleAttacks);
+    super(scene, x, y, 'sword', allowedMoves, allowedAttacks);
     scene.add.existing(this);
+    this.life = 1;
   }
 }
 
 export class Staff extends Piece {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'staff', [
+  constructor(scene, x, y, turn) {
+    matrizM = [
       { row: 1, col: 0 }, 
       { row: 0, col: 1 },
       { row: 1, col: 1 },
@@ -98,8 +122,40 @@ export class Staff extends Piece {
       { row: -1, col: -1 },
       { row: -1, col: 0 },
       { row: 0, col: -1 }
-    ]);
+    ];
+    matrizA = [
+      { rowA: 1, colA: 0 }, 
+      { rowA: 0, colA: 1 },
+      { rowA: 1, colA: 1 },
+      { rowA: 1, colA: -1 },
+      { rowA: -1, colA: 1 },
+      { rowA: -1, colA: -1 },
+      { rowA: -1, colA: 0 },
+      { rowA: 0, colA: -1 },
+      { rowA: 0, colA: 2},
+      { rowA: 1, colA: 2},
+      { rowA: -1, colA: 2},
+      { rowA: 0, colA: -2},
+      { rowA: 1, colA: -2},
+      { rowA: -1, colA: -2},
+      { rowA: 2, colA: 0},
+      { rowA: 2, colA: 1},
+      { rowA: 2, colA: -1},
+      { rowA: -2, colA: 0},
+      { rowA: -2, colA: 1},
+      { rowA: -2, colA: -1},
+    ];
+
+    if (turn == 0) {
+      allowedMoves = matrizM;
+      allowedAttacks = matrizA;
+    } else if (turn == 1) {
+      allowedMoves = matrizM;
+      allowedAttacks = matrizA;
+    }
+    super(scene, x, y, 'staff', allowedMoves, allowedAttacks);
     scene.add.existing(this);
+    this.life = 1;
   }
 }
 
@@ -114,7 +170,15 @@ export class Shield extends Piece {
       { row: -1, col: -1 },
       { row: -1, col: 0 },
       { row: 0, col: -1 }
-    ]);
+    ], [ { rowA: 1, colA: 0 }, 
+      { rowA: 0, colA: 1 },
+      { rowA: 1, colA: 1 },
+      { rowA: 1, colA: -1 },
+      { rowA: -1, colA: 1 },
+      { rowA: -1, colA: -1 },
+      { rowA: -1, colA: 0 },
+      { rowA: 0, colA: -1 }]);
     scene.add.existing(this);
+    this.life = 1;
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+// Posição da peça original precisa ficar como occupied = false;
+
+
 import { Crown, Lance, Staff, Sword, Shield } from './PieceClasses.js';
 
 var config = {
@@ -20,8 +23,7 @@ let downPieces;
 let allPieces;
 let player = 0;
 let pieces = 0;
-let enemyPieces = 0;
-
+let count = 0;
 
 function preload() {
   this.load.image('black', 'assets/black.png');
@@ -47,8 +49,8 @@ function createBoard() {
     board[i] = [];
     for (var j = 0; j < boardSize; j++) {
       var tileColor = (i + j) % 2 === 0 ? 'black' : 'white';
-      var tile = this.add.image(j * tileSize, i * tileSize, tileColor).setOrigin(0);
-      board[i][j] = { tile: tile, piece: null };
+      var tile = this.add.image(j * tileSize, i * tileSize, tileColor).setOrigin(0,0);
+      board[i][j] = { tile: tile, piece: null, occupied: false };
     }
   }
 }
@@ -63,42 +65,46 @@ function selectPieces() {
   });
 
   pieces = player === 0 ? topPieces : downPieces;
-  enemyPieces = player === 0 ? downPieces : topPieces;
-
   pieces.children.iterate((piece) => {
     piece.setInteractive({ draggable: true });
 
     piece.on('dragstart', () => {
       piece.setTint(0xffd700);
-      showValidMoves(piece);
+      showMoves(piece);
+      showAttacks(piece);
+      console.log(piece)
     });
 
     piece.on('drag', (pointer, dragX, dragY) => {
       piece.x = dragX;
       piece.y = dragY;
+      
     });
 
     piece.on('dragend', () => {
       piece.clearTint();
       const newX = piece.x;
       const newY = piece.y;
-      const newPosition = getBoardPosition(newX, newY);
-      if (isValidMove(piece, newPosition)) {
-        movePieceToPosition(piece, newPosition);
-        attack(); 
-        player = 1 - player;
-        selectPieces();
-      } else {
-        movePieceToPosition(piece, piece.originalPosition);
-      }
-      hideValidMoves();
+      const newPosition = getPosition(newX, newY);
+      
+        if (isValidMove(piece, newPosition)) {
+          movePiece(piece, newPosition);
+          hideAttacks(piece);
+          player = 1 - player;
+          selectPieces();
+          
+        } else {
+          movePiece(piece, piece.originalPosition);
+        }
+
+      hideMoves();
+      
     });
     piece.originalPosition = { row: Math.floor(piece.y / tileSize), col: Math.floor(piece.x / tileSize) };
   });
 }
 
-function showValidMoves(piece) {
-  piece.
+function showMoves(piece) {
   piece.allowedMoves.forEach((move) => {
     const newRow = piece.originalPosition.row + move.row;
     const newCol = piece.originalPosition.col + move.col;
@@ -107,51 +113,158 @@ function showValidMoves(piece) {
       const tileColor = tile.texture.key;
 
       if (tileColor === 'black') {
-        tile.setTint(0x3ee500); 
+        tile.setTint(0x3ee500);
       } else if (tileColor === 'white') {
-        tile.setTint(0x04b700);  
+        tile.setTint(0x04b700);
       }
     }
-  });
+  });  
 }
 
+function showAttacks(piece) {
+  const enemyPieces = player === 0 ? downPieces : topPieces;
+  const allyPieces = player === 0 ? topPieces : downPieces;
 
-function attack(piece, enemyPieces) {
-  piece.possibleAttacks.forEach((attack) => {
-    const newRow = piece.originalPosition.row + attack.row;
-    const newCol = piece.originalPosition.col + attack.col;  
-    
-    
-  })
-}
-
-
-function hideValidMoves() {
-  board.forEach((row) => {
-    row.forEach((cell) => {
-      cell.tile.clearTint();
+  if (piece.texture.key !== "shield") {
+    enemyPieces.children.iterate((enemyPiece) => {
+      if (enemyPiece.player !== piece.player) {
+        if (attackRange(piece, enemyPiece)) {
+          enemyPiece.setTint(0xff0000);
+        }
+      }
     });
-  });
+  } else if (piece.texture.key === "shield") {
+    allyPieces.children.iterate((allyPiece) => {
+      if (allyPiece.player === piece.player) {
+        if (attackRange(piece, allyPiece)) {
+          allyPiece.setTint(0x3232ff);
+        }
+      }
+    });
+  }
 }
 
-function isValidMove(piece, newPosition) {
-  return piece.allowedMoves.some(
-    (move) =>
-      newPosition.row === piece.originalPosition.row + move.row &&
-      newPosition.col === piece.originalPosition.col + move.col
+function attackRange(piece, enemyPiece) {
+  const { row, col } = getPosition(enemyPiece.x, enemyPiece.y);
+
+  return piece.allowedAttacks.some(
+    (attack) =>
+      row === piece.originalPosition.row + attack.rowA &&
+      col === piece.originalPosition.col + attack.colA
   );
 }
 
-function getBoardPosition(x, y) {
+function isValidMove(piece, newPosition) {
+  const { row, col } = newPosition;
+
+  let targetPiece = board[row][col].piece;
+
+  if (board[row][col].occupied) {
+    if (piece.texture.key !== "shield") {
+      if (targetPiece.player !== piece.player) {
+        
+        return piece.allowedAttacks.some(
+          (move) =>
+            newPosition.row === piece.originalPosition.row + move.rowA &&
+            newPosition.col === piece.originalPosition.col + move.colA
+        );
+      } else if (targetPiece.player === piece.player) {
+        return false;
+      }
+    } else if (piece.texture.key === "shield") {
+      
+      if (targetPiece.player === piece.player) {
+        return piece.allowedAttacks.some(
+          (move) =>
+            newPosition.row === piece.originalPosition.row + move.rowA &&
+            newPosition.col === piece.originalPosition.col + move.colA
+        );
+      } else if (targetPiece.player !== piece.player) {
+        return false;
+      }
+    }
+  } else {
+    return piece.allowedMoves.some(
+      (move) =>
+        newPosition.row === piece.originalPosition.row + move.row &&
+        newPosition.col === piece.originalPosition.col + move.col
+    );
+  }
+}
+
+function movePiece(piece, newPosition) {
+  const { row, col } = newPosition;
+
+  if (count == 0) {
+    piece.x = col * tileSize + tileSize / 2;
+    piece.y = row * tileSize + tileSize / 2;
+
+    board[row][col].piece = piece;
+    board[row][col].occupied = true;
+
+    count++;
+
+  } else {
+    let targetPiece = board[row][col].piece;
+
+    if (board[row][col].occupied) {
+      if (piece.texture.key !== "shield") {
+        if (targetPiece.player !== piece.player) {
+          if (attackRange(piece, targetPiece)) {
+            targetPiece.life -= 1;
+            if (targetPiece.life == 0) {
+              targetPiece.destroy();
+              piece.x = col * tileSize + tileSize / 2;
+              piece.y = row * tileSize + tileSize / 2;
+              board[row][col].piece = piece;
+              board[row][col].occupied = true;
+            }
+          }
+        }
+      } else if (piece.texture.key === "shield") {
+
+        if (targetPiece.player === piece.player) {
+          if (attackRange(piece, targetPiece)) {
+            targetPiece.life += 1;
+            piece.destroy();
+
+          }
+        }
+      }
+    }
+    piece.x = col * tileSize + tileSize / 2;
+    piece.y = row * tileSize + tileSize / 2;
+
+    board[row][col].piece = piece;
+    board[row][col].occupied = true;
+
+  }
+}
+
+function getPosition(x, y) {
   const row = Math.floor(y / tileSize);
   const col = Math.floor(x / tileSize);
   return { row, col };
 }
 
-function movePieceToPosition(piece, newPosition) {
-  const { row, col } = newPosition;
-  piece.x = col * tileSize + tileSize / 2;
-  piece.y = row * tileSize + tileSize / 2;
+function hideAttacks(piece) {
+
+  if (piece.texture.key !== 'shield') {
+    const targetPieces = player === 0 ? downPieces : topPieces;
+    targetPieces.children.iterate((target) => target.clearTint());
+    
+  } else if (piece.texture.key == 'shield') {
+    const targetPieces = player === 0 ? topPieces : downPieces;
+    targetPieces.children.iterate((target) => target.clearTint());
+  }
+}
+
+function hideMoves() {
+  board.forEach((row) => {
+    row.forEach((cell) => {
+      cell.tile.clearTint();
+    });
+  });
 }
 
 function placePieces() {
@@ -178,6 +291,16 @@ function placePieces() {
     new Lance(this, 4 * tileSize + tileSize / 2, 5 * tileSize + tileSize / 2, 1),
     new Crown(this, 3 * tileSize + tileSize / 2, 6 * tileSize + tileSize / 2, 1)
   ])
+
+  topPieces.children.iterate((piece) => {
+    movePiece(piece, getPosition(piece.x, piece.y));
+    piece.player = 0;
+  });
+
+  downPieces.children.iterate((piece) => {
+    movePiece(piece, getPosition(piece.x, piece.y));
+    piece.player = 1;
+  });
 
   allPieces = [topPieces, downPieces];
 
